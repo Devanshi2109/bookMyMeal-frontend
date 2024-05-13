eed1eaaf2eaa4690a87621cf8d7df2c9e7078a0e
Anuj Rai <anujrai1701@gmail.com>
2024-05-13 15:44:47 +0530

    Add calender to the home page using react-calendar

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const CalendarComponent = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    };
+
+    return (
+        <div className="app container mx-auto mt-5">
+            <div className="calendar-container mx-auto">
+                <Calendar
+                    onChange={handleDateChange}
+                    value={selectedDate}
+                    className="w-full sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl 2xl:max-w-5xl bg-white bg-opacity-90 border border-gray-300 font-sans leading-normal shadow-md rounded-lg p-4 transition-colors duration-200 h-auto sm:h-auto md:h-auto lg:h-auto xl:h-auto 2xl:h-auto text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl"
+                    
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CalendarComponent;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import Calendar from './Calendar';
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <div className="mx-auto flex flex-col justify-center items-center p-4" >
+        <Calendar />
+      </div>
       <Footer />
     </div>
   );
