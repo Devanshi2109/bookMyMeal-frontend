9112aa9225c831dac68acb9fd34d64a86d70e891
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2024-05-13 16:54:37 +0530

    Add Basic Button For Book a meal ,cancel a meal, quick book and View Booking for meals

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,20 @@
-import React from "react";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
+// import Footer from "./Footer";
+import BookAMealBtn from "./BookAMealBtn";
+import CancelAMealBtn from "./CancelAMealBtn";
+import QuickBookBtn from "./QuickBookBtn";
+import ViewBookBtn from "./ViewBookBtn";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <Footer />
+      <div className="flex space-x-4">
+        <CancelAMealBtn />
+        <BookAMealBtn />
+        <ViewBookBtn />
+        <QuickBookBtn />
+      </div>
     </div>
   );
 };
---
+++ src/components/ViewBookBtn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ViewBookBtn = () => {
+  return (
+    <button className="fixed px-5 py-3 m-2 text-white transform -translate-y-1/2 bg-blue-600 rounded-lg shadow-md top-1/4 left-50">
+      View Booking
+    </button>
+  );
+};
+
+export default ViewBookBtn;
