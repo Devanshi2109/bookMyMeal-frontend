9fed6be92217771a42f628f1a111b180dc5f4ecc
Anuj Rai <anujrai1701@gmail.com>
2024-05-17 06:39:25 +0530

    Fixed the layout for forgot-password and otp and added the timer for resend otp in the OTP component

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import forgotPasswordImg from "../assest/images/signup-image.jpg";
+import AuthLayout from "./AuthLayout";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -25,56 +25,49 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <div className="hidden w-2/3 bg-center bg-cover md:block">
-        <img src={forgotPasswordImg} alt="Forgot Password" />
+    <AuthLayout>
+      <div className="mb-8 text-center">
+        <h1 className="mb-4 text-3xl font-bold">Forgot Your Password?</h1>
+        <p className="mb-4 text-gray-500">
+          Enter your email address to reset your password.
+        </p>
       </div>
-      <div className="flex items-center justify-center w-full p-8 md:w-1/2">
-        <div className="w-full max-w-md">
-          <div className="mb-10 text-center">
-            <h1 className="mb-4 text-3xl font-bold">Forgot Your Password?</h1>
-            <p className="mb-4 text-gray-500">
-              Enter your email address to reset your password.
-            </p>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email Address
-              </label>
-              <input
-                id="email"
-                type="email"
-                className="block w-full px-4 py-3 mt-1 text-sm border border-gray-300 rounded form-input"
-                placeholder="Your Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="flex items-center justify-between mb-4">
-              <button
-                type="submit"
-                className="w-full px-4 py-3 text-sm text-white bg-orange-600 rounded-md hover:bg-orange-700"
-              >
-                Reset Password
-              </button>
-            </div>
-            <div className="mb-0 text-center">
-              <p className="text-sm">
-                Remember your password?{" "}
-                <Link to="/login" className="text-blue-600 hover:underline">
-                  Sign in
-                </Link>
-              </p>
-            </div>
-          </form>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="email" 
+            className="block text-sm font-medium text-gray-700"
+            >
+              Email Address
+          </label>
+          <input 
+            id="email" 
+            type="email" 
+            className="block w-full px-4 py-3 mt-1 text-sm border border-gray-300 rounded form-input" 
+            placeholder="Your Email Address" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
         </div>
-      </div>
+        <div className="flex items-center justify-between mb-4">
+          <button
+            type="submit" 
+            className="w-full px-4 py-3 text-sm text-white bg-orange-600 rounded-md hover:bg-orange-700"
+            >
+              Reset Password
+            </button>
+        </div>
+        <div className="mb-0 text-center">
+          <p className="text-sm">
+            Remember your password? 
+            <Link to="/login" className="text-blue-600 hover:underline">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </form>
       <Toaster position="top-right" />
-    </div>
+    </AuthLayout>
   );
 };
 
