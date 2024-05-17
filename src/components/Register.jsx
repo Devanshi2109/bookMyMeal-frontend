8a4ab68244ed07158b7bcd3d8224af3023665aff
Anuj Rai <anujrai1701@gmail.com>
2024-05-17 05:45:42 +0530

    Fixed Register by using AuthLayout and made it responsive while also added show password

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import signUpImg from "../assest/images/signup-image.jpg";
 import { toast, Toaster } from "react-hot-toast";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import AuthLayout from "./AuthLayout";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,6 +11,8 @@ const Register = () => {
     pass: "",
     re_pass: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRePassword, setShowRePassword] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -49,126 +52,129 @@ const Register = () => {
   };
 
   return (
-    <div className="flex h-screen">
-      <div className="hidden w-2/3 bg-center bg-cover md:block">
-        <img src={signUpImg} alt="" />
+    <AuthLayout>
+      <div className="mb-8 text-center">
+        <p className="mb-2 text-2xl font-bold text-left">Sign up</p>
+        <p className="text-left text-gray-500">
+          Create your account to get started.
+          </p>
       </div>
-      <div className="flex items-center justify-center w-full p-8 md:w-1/2">
-        <div className="w-full max-w-md">
-          <div className="mb-10 text-center">
-            <div className="flex items-start justify-start mx-auto text-left mb-14">
-              {/* Your logo and title */}
-            </div>
-            <p className="mb-4 text-3xl font-bold text-left">Sign up</p>
-            <p className="mb-4 text-left text-gray-500">
-              Create your account to get started.
-            </p>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Name
-              </label>
-              <input
-                id="name"
-                type="text"
-                className="block w-full px-4 py-3 mt-1 text-sm border border-gray-300 rounded form-input"
-                value={formData.name}
-                onChange={handleChange}
-                name="name"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                className="block w-full px-4 py-3 mt-1 text-sm border border-gray-300 rounded form-input"
-                value={formData.email}
-                onChange={handleChange}
-                name="email"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="pass"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <input
-                id="pass"
-                type="password"
-                className="block w-full px-4 py-3 mt-1 text-sm border border-gray-300 rounded form-input"
-                value={formData.pass}
-                onChange={handleChange}
-                name="pass"
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="re_pass"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Confirm Password
-              </label>
-              <input
-                id="re_pass"
-                type="password"
-                className="block w-full px-4 py-3 mt-1 text-sm border border-gray-300 rounded form-input"
-                value={formData.re_pass}
-                onChange={handleChange}
-                name="re_pass"
-              />
-            </div>
-            <div className="flex items-center justify-between mb-4">
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  id="terms"
-                  className="w-4 h-4 text-indigo-600 transition duration-150 ease-in-out form-checkbox"
-                />
-                <label
-                  htmlFor="terms"
-                  className="block ml-2 text-sm text-gray-900"
-                >
-                  I agree to the{" "}
-                  <Link to="/terms" className="text-blue-600 hover:underline">
-                    Terms of Service
-                  </Link>
-                </label>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="w-full px-4 py-3 text-sm text-white bg-orange-600 rounded-md hover:bg-orange-700"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-          <div className="mt-4 text-center">
-            <p className="text-sm">
-              Already have an account?{" "}
-              <Link to="/login" className="text-blue-600 hover:underline">
-                Sign in
-              </Link>
-            </p>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <label 
+            htmlFor="name" 
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input 
+            id="name" 
+            type="text" 
+            className="block w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded form-input" 
+            value={formData.name} 
+            onChange={handleChange} 
+            name="name" 
+          />
+        </div>
+        <div className="mb-2">
+          <label 
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+              Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            className="block w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded form-input"
+            value={formData.email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+        <div className="mb-2">
+          <label
+            htmlFor="pass"
+            className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+          <div className="relative">
+            <input
+              id="pass"
+              type={showPassword ? "text" : "password"}
+              className="block w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded form-input"
+              value={formData.pass}
+              onChange={handleChange}
+              name="pass"
+            />
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
           </div>
         </div>
+        <div className="mb-2">
+          <label
+            htmlFor="re_pass"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Confirm Password
+          </label>
+          <div className="relative">
+            <input
+              id="re_pass"
+              type={showRePassword ? "text" : "password"}
+              className="block w-full px-3 py-2 mt-1 text-sm border border-gray-300 rounded form-input"
+              value={formData.re_pass}
+              onChange={handleChange}
+              name="re_pass"
+            />
+            <span
+              onClick={() => setShowRePassword(!showRePassword)}
+              className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
+              {showRePassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+        </div>
+        <div className="flex items-center justify-between mb-2">
+          <div className="flex items-center">
+            <input 
+              type="checkbox"
+              id="terms" 
+              className="w-4 h-4 text-indigo-600 transition duration-150 ease-in-out form-checkbox" 
+            />
+            <label 
+              htmlFor="terms" 
+              className="block ml-2 text-sm text-gray-900"
+            >
+              I agree to the{" "}
+              <Link to="/terms" className="text-blue-600 hover:underline">
+                Terms of Service
+              </Link>
+            </label>
+          </div>
+        </div>
+        <div>
+          <button
+            type="submit" 
+            className="w-full px-4 py-2 text-sm text-white bg-orange-600 rounded-md hover:bg-orange-700"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+      <div className="mt-4 text-center">
+        <p className="text-sm">
+          Already have an account?{" "} 
+          <Link to="/login" className="text-blue-600 hover:underline">
+            Sign in
+          </Link>
+        </p>
       </div>
       <Toaster position="top-right" />
-    </div>
+    </AuthLayout>
   );
 };
 
